2b2d262c8b51eceab600395f0732fb8563654a9c
barsh <michael.barshinger@profiscience.com>
2016-04-24 18:21:25 -0500

    Create captivate-utils.js

---
+++ captivate-utils.js
@@ -0,0 +1,31 @@
+cp.utils = {
+	printInteractions: function() {
+		var interactions = []
+		Object.keys(cp.model.data).forEach(
+			function (key) { 
+				var element = cp.model.data[key]; 
+				if (element.iid) {
+					interactions.push({slideName: element.sn, elementId: element.gn, interactionId: element.iid})
+				} 
+			}
+		)
+		console.table(interactions)
+	}, 
+	markAllSlidesAsViewed: function() {
+		cp.movie.playbackController.m_slideNames.forEach(
+			function (slideName) {
+				cp.model.data[slideName].v = true
+			}
+		)		
+	},
+	disablePlaybarSlider: function() {
+		document.getElementsByClassName('playbarSliderThumb')[0].style.display='none'
+		if (!cp.utils._jumpToFrame) cp.utils._jumpToFrame = cp.playbar.mainMovie.jumpToFrame
+		cp.playbar.mainMovie.jumpToFrame = function() {}
+	},
+	enablePlaybarSlider: function() {
+		document.getElementsByClassName('playbarSliderThumb')[0].style.display=''
+		if (cp.utils._jumpToFrame) cp.playbar.mainMovie.jumpToFrame = cp.utils._jumpToFrame
+	},
+  _jumpToFrame: null
+}
