ae2303aa66a65980c6cc1f1fb78844fcfb6bc1ba
barsh <michael.barshinger@profiscience.com>
2017-02-01 10:47:24 -0600

    fix for IE11

--- disable-playbar-slider.js
+++ disable-playbar-slider.js
@@ -4,7 +4,7 @@ document.getElementsByClassName('playbarSliderThumb')[0].style.display='none'
 if (!cp.playbar.mainMovie._jumpToFrame) {
   cp.playbar.mainMovie._jumpToFrame = cp.playbar.mainMovie.jumpToFrame
   cp.playbar.mainMovie.jumpToFrame = function(a) {
-    var stack = new Error().stack
+    var stack = new Error().stack || ''
     var callerIsNotPlaybar =
       stack.indexOf('HTMLCanvasElement.moveSlider') == -1
       && stack.indexOf('PlayBarSlider.moveSlider') == -1
