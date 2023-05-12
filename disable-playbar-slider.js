f98379ade0bcb69c2ed30b37f7786517455b8cd7
Serena Capodicasa <capodicasa.serena@gmail.com>
2023-05-12 14:07:11 +0200

    Update disable-playbar-slider.js - avoid "pause" when user clicks on the playbar
    Hi, thanks a lot for this script.
    I'm not a dev but with my basic knowledge yesterday I analysed the CPM.js and found out the code to avoid the the "pause" of the project when the user accidently clicks on the playbar. It works.
    I hope it could be helpful.

--- disable-playbar-slider.js
+++ disable-playbar-slider.js
@@ -1,13 +1,19 @@
-// disable playbar slider
+// hide the slider thumbnail
+var thumbEl=document.getElementsByClassName('playbarSliderThumb')[0]
+thumbEl.style.display='none'
 
-document.getElementsByClassName('playbarSliderThumb')[0].style.display='none'
-if (!cp.playbar.mainMovie._jumpToFrame) {
-  cp.playbar.mainMovie._jumpToFrame = cp.playbar.mainMovie.jumpToFrame
-  cp.playbar.mainMovie.jumpToFrame = function(a) {
-    var stack = new Error().stack || ''
+// disable drag and click slider navigation
+if (!cp.PB.mainMovie._jumpToFrame) {
+  cp.PB.mainMovie._jumpToFrame =     cp.PB.mainMovie.jumpToFrame
+  cp.PB.mainMovie.jumpToFrame = function(a) {
+    var stack = new Error().stack
     var callerIsNotPlaybar =
       stack.indexOf('HTMLCanvasElement.moveSlider') == -1
       && stack.indexOf('PlayBarSlider.moveSlider') == -1
-    if (callerIsNotPlaybar) cp.playbar.mainMovie._jumpToFrame.call(cp.playbar.mainMovie, a)
+    if (callerIsNotPlaybar)
+      cp.PB.mainMovie._jumpToFrame.call(cp.PB.mainMovie, a)
+// avoid the "pause" when the user clicks on the playbar
+cp.PB.mainMovie.play(cp.ReasonForPlay.PLAYBAR_ACTION)
+
   }
 }
